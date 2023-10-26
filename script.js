9a91f682b2fb4614c48c9c4f288318cf3520b995
Willy Prado <pradopublicity@gmail.com>
2023-10-27 00:51:40 +0200

    Adding cards random in home + style

--- script.js
+++ script.js
@@ -50,3 +50,76 @@ getCharacters()
 const date = new Date();
 const year = date.getFullYear();
 document.getElementById("year").innerHTML = year;
+
+// Card Personajes Random
+async function getRandomCharacters() {
+  const randomCharacterIds = [];
+  while (randomCharacterIds.length < 3) {
+    const randomId = Math.floor(Math.random() * 826) + 1;
+    if (!randomCharacterIds.includes(randomId)) {
+      randomCharacterIds.push(randomId);
+    }
+  }
+
+  const characterData = [];
+
+  for (const id of randomCharacterIds) {
+    try {
+      const response = await fetch(`${baseUrl}character/${id}`);
+      const data = await response.json();
+      characterData.push(data);
+    } catch (error) {
+      console.error(`Error al obtener el personaje ${id}:`, error);
+    }
+  }
+
+  return characterData;
+}
+
+async function displayRandomCharacters() {
+  const characters = await getRandomCharacters();
+
+  characters.forEach((character, index) => {
+    const characterCard = `
+                  <div id="character-card" class="character-card">
+                    <div class="card-header">
+                      <div class="card-image">
+                        <img src=${character.image} />
+                      </div>
+                      <div class="character-card-title">
+                        <h2 class="character-card-name">${character.name}</h2>
+                        <p class="character-card-description">ID:${
+                          character.id
+                        } | Location:${character.location.name}</p>
+                      </div>
+                    </div>
+                    <div class="card-info">
+                      <div class="character-card-text-wrapper">
+                        <span>TYPE</span>
+                        <p>${character.type || "Undefined"}</p>
+                      </div>
+                      <div class="character-card-text-wrapper">
+                        <span>STATUS</span>
+                        <p>${character.status}</p>
+                      </div>
+                      <div class="character-card-text-wrapper">
+                        <span>SPECIES</span>
+                        <p>${character.species}</p>
+                      </div>
+                      <div class="character-card-text-wrapper">
+                        <span>GENDER</span>
+                        <p>${character.gender}</p>
+                      </div>
+                      <div class="character-card-text-wrapper">
+                        <span>ORIGIN</span>
+                        <p>${character.origin.name}</p>
+                      </div>
+                    </div>
+                  </div>
+              `;
+    const contents = document.getElementById("content");
+    contents.innerHTML += characterCard;
+  });
+}
+
+displayRandomCharacters();
