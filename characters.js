d3e5fb95dac7a05236579858a43a44c6121067cd
Willy Prado <pradopublicity@gmail.com>
2023-10-27 02:24:00 +0200

    Fix characters js

--- characters.js
+++ characters.js
@@ -1,5 +1,5 @@
 let currentPage = 1;
-const contentElement = document.getElementById("content");
+const contentElement = document.getElementById("content-characters");
 const nextButton = document.getElementById("btnNext");
 const prevButton = document.getElementById("btnPrev");
 
@@ -24,40 +24,40 @@ const createCharacterCard = character => {
 
   return `
   <div id="character-card" class="character-card">
-  <div class="card-header">
-    <div class="card-image">
-      <img src=${image} />
-    </div>
-    <div class="character-card-title">
-      <h2 class="character-card-name">${name}</h2>
-      <p class="character-card-description">ID:${id} | Location:${
+    <div class="card-header">
+      <div class="card-image">
+        <img src=${image} />
+      </div>
+      <div class="character-card-title">
+        <h2 class="character-card-name">${name}</h2>
+        <p class="character-card-description">ID:${id} | Location:${
     location.name
   }</p>
+      </div>
+    </div>
+    <div class="card-info">
+      <div class="character-card-text-wrapper">
+        <span>TYPE</span>
+        <p>${type || "Undefined"}</p>
+      </div>
+      <div class="character-card-text-wrapper">
+        <span>STATUS</span>
+        <p>${status}</p>
+      </div>
+      <div class="character-card-text-wrapper">
+        <span>SPECIES</span>
+        <p>${species}</p>
+      </div>
+      <div class="character-card-text-wrapper">
+        <span>GENDER</span>
+        <p>${gender}</p>
+      </div>
+      <div class="character-card-text-wrapper">
+        <span>ORIGIN</span>
+        <p>${origin.name}</p>
+      </div>
     </div>
   </div>
-  <div class="card-info">
-    <div class="character-card-text-wrapper">
-      <span>TYPE</span>
-      <p>${type || "Undefined"}</p>
-    </div>
-    <div class="character-card-text-wrapper">
-      <span>STATUS</span>
-      <p>${status}</p>
-    </div>
-    <div class="character-card-text-wrapper">
-      <span>SPECIES</span>
-      <p>${species}</p>
-    </div>
-    <div class="character-card-text-wrapper">
-      <span>GENDER</span>
-      <p>${gender}</p>
-    </div>
-    <div class="character-card-text-wrapper">
-      <span>ORIGIN</span>
-      <p>${origin.name}</p>
-    </div>
-  </div>
-</div>
   `;
 };
 
