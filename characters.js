68d34fb16ceb2160f1a78ba8fcde5a7c4e3e7d7a
Willy Prado <pradopublicity@gmail.com>
2023-10-27 02:04:23 +0200

    Adding characters page with all characters and pagination

---
+++ characters.js
@@ -0,0 +1,90 @@
+let currentPage = 1;
+const contentElement = document.getElementById("content");
+const nextButton = document.getElementById("btnNext");
+const prevButton = document.getElementById("btnPrev");
+
+const fetchCharacterData = page => {
+  return fetch(`https://rickandmortyapi.com/api/character/?page=${page}`).then(
+    response => response.json()
+  );
+};
+
+const createCharacterCard = character => {
+  const {
+    image,
+    name,
+    status,
+    species,
+    gender,
+    origin,
+    id,
+    location,
+    type = "Classified",
+  } = character;
+
+  return `
+  <div id="character-card" class="character-card">
+  <div class="card-header">
+    <div class="card-image">
+      <img src=${image} />
+    </div>
+    <div class="character-card-title">
+      <h2 class="character-card-name">${name}</h2>
+      <p class="character-card-description">ID:${id} | Location:${
+    location.name
+  }</p>
+    </div>
+  </div>
+  <div class="card-info">
+    <div class="character-card-text-wrapper">
+      <span>TYPE</span>
+      <p>${type || "Undefined"}</p>
+    </div>
+    <div class="character-card-text-wrapper">
+      <span>STATUS</span>
+      <p>${status}</p>
+    </div>
+    <div class="character-card-text-wrapper">
+      <span>SPECIES</span>
+      <p>${species}</p>
+    </div>
+    <div class="character-card-text-wrapper">
+      <span>GENDER</span>
+      <p>${gender}</p>
+    </div>
+    <div class="character-card-text-wrapper">
+      <span>ORIGIN</span>
+      <p>${origin.name}</p>
+    </div>
+  </div>
+</div>
+  `;
+};
+
+const loadPage = page => {
+  fetchCharacterData(page).then(data => {
+    contentElement.innerHTML = ""; // Limpiar contenido anterior
+
+    data.results.forEach(character => {
+      const card = createCharacterCard(character);
+      contentElement.insertAdjacentHTML("beforeend", card);
+    });
+
+    currentPage = page;
+    updatePageButtons(data.info.prev, data.info.next);
+  });
+};
+
+const updatePageButtons = (prevPage, nextPage) => {
+  prevButton.disabled = !prevPage;
+  nextButton.disabled = !nextPage;
+
+  prevButton.onclick = () => prevPage && loadPage(currentPage - 1);
+  nextButton.onclick = () => nextPage && loadPage(currentPage + 1);
+};
+
+loadPage(currentPage);
+
+// Get current year and display it
+const year = new Date().getFullYear();
+document.getElementById("year").innerHTML = year;
--- script.js
+++ script.js
@@ -1,125 +1,113 @@
-let charactersData;
-let locationsData;
-let episodesData;
-
 const baseUrl = "https://rickandmortyapi.com/api/";
 
-const getCharacters = async () => {
+// Helper function to make API requests
+async function fetchData(endpoint) {
   try {
-    const response = await fetch(`${baseUrl}character`);
-    charactersData = await response.json();
-    console.log(charactersData);
+    const response = await fetch(`${baseUrl}${endpoint}`);
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`);
+    }
+    return await response.json();
   } catch (error) {
-    console.error("Error al obtener los personajes:", error);
+    throw new Error(`Error fetching data from ${endpoint}: ${error.message}`);
   }
-};
+}
 
-const getLocations = async () => {
+// Fetch characters, locations, and episodes data
+async function fetchAllData() {
   try {
-    const response = await fetch(`${baseUrl}location`);
-    locationsData = await response.json();
-    console.log(locationsData);
+    const [charactersData, locationsData, episodesData] = await Promise.all([
+      fetchData("character"),
+      fetchData("location"),
+      fetchData("episode"),
+    ]);
+    return [charactersData, locationsData, episodesData];
   } catch (error) {
-    console.error("Error al obtener las ubicaciones:", error);
+    throw new Error(`Error fetching all data: ${error.message}`);
   }
-};
+}
 
-const getEpisodes = async () => {
+// Display counts of characters, locations, and episodes
+async function displayDataCounts() {
   try {
-    const response = await fetch(`${baseUrl}episode`);
-    episodesData = await response.json();
-    console.log(episodesData);
-  } catch (error) {
-    console.error("Error al obtener los episodios:", error);
-  }
-};
-
-getCharacters()
-  .then(() => getLocations())
-  .then(() => getEpisodes())
-  .then(() => {
+    const [charactersData, locationsData, episodesData] = await fetchAllData();
     document.getElementById("countApi").innerHTML += `<p>
-    CHARACTERS: ${charactersData.info.count} | LOCATIONS: ${locationsData.info.count} | EPISODES: ${episodesData.info.count}
-  </p>`;
-  })
-  .catch(error => {
-    console.error("Error al obtener los datos:", error);
-  });
+      CHARACTERS: ${charactersData.info.count} | LOCATIONS: ${locationsData.info.count} | EPISODES: ${episodesData.info.count}
+    </p>`;
+  } catch (error) {
+    console.error("Error displaying data counts:", error);
+  }
+}
 
-// Año del Footer
-const date = new Date();
-const year = date.getFullYear();
+// Get current year and display it
+const year = new Date().getFullYear();
 document.getElementById("year").innerHTML = year;
 
-// Card Personajes Random
-async function getRandomCharacters() {
-  const randomCharacterIds = [];
-  while (randomCharacterIds.length < 3) {
-    const randomId = Math.floor(Math.random() * 826) + 1;
-    if (!randomCharacterIds.includes(randomId)) {
-      randomCharacterIds.push(randomId);
-    }
-  }
-
-  const characterData = [];
-
-  for (const id of randomCharacterIds) {
-    try {
-      const response = await fetch(`${baseUrl}character/${id}`);
-      const data = await response.json();
-      characterData.push(data);
-    } catch (error) {
-      console.error(`Error al obtener el personaje ${id}:`, error);
-    }
-  }
-
-  return characterData;
-}
-
+// Display random character cards
 async function displayRandomCharacters() {
-  const characters = await getRandomCharacters();
+  try {
+    const randomCharacterIds = new Set();
+    while (randomCharacterIds.size < 3) {
+      const randomId = Math.floor(Math.random() * 826) + 1;
+      randomCharacterIds.add(randomId);
+    }
 
-  characters.forEach((character, index) => {
-    const characterCard = `
-                  <div id="character-card" class="character-card">
-                    <div class="card-header">
-                      <div class="card-image">
-                        <img src=${character.image} />
-                      </div>
-                      <div class="character-card-title">
-                        <h2 class="character-card-name">${character.name}</h2>
-                        <p class="character-card-description">ID:${
-                          character.id
-                        } | Location:${character.location.name}</p>
-                      </div>
-                    </div>
-                    <div class="card-info">
-                      <div class="character-card-text-wrapper">
-                        <span>TYPE</span>
-                        <p>${character.type || "Undefined"}</p>
-                      </div>
-                      <div class="character-card-text-wrapper">
-                        <span>STATUS</span>
-                        <p>${character.status}</p>
-                      </div>
-                      <div class="character-card-text-wrapper">
-                        <span>SPECIES</span>
-                        <p>${character.species}</p>
-                      </div>
-                      <div class="character-card-text-wrapper">
-                        <span>GENDER</span>
-                        <p>${character.gender}</p>
-                      </div>
-                      <div class="character-card-text-wrapper">
-                        <span>ORIGIN</span>
-                        <p>${character.origin.name}</p>
-                      </div>
-                    </div>
-                  </div>
-              `;
-    const contents = document.getElementById("content");
-    contents.innerHTML += characterCard;
-  });
+    const characterData = await Promise.all(
+      [...randomCharacterIds].map(id => fetchData(`character/${id}`))
+    );
+
+    characterData.forEach((character, index) => {
+      const characterCard = `
+      <div id="character-card" class="character-card">
+        <div class="card-header">
+          <div class="card-image">
+            <img src=${character.image} />
+          </div>
+          <div class="character-card-title">
+            <h2 class="character-card-name">${character.name}</h2>
+            <p class="character-card-description">ID:${
+              character.id
+            } | Location:${character.location.name}</p>
+          </div>
+        </div>
+        <div class="card-info">
+          <div class="character-card-text-wrapper">
+            <span>TYPE</span>
+            <p>${character.type || "Undefined"}</p>
+          </div>
+          <div class="character-card-text-wrapper">
+            <span>STATUS</span>
+            <p>${character.status}</p>
+          </div>
+          <div class="character-card-text-wrapper">
+            <span>SPECIES</span>
+            <p>${character.species}</p>
+          </div>
+          <div class="character-card-text-wrapper">
+            <span>GENDER</span>
+            <p>${character.gender}</p>
+          </div>
+          <div class="character-card-text-wrapper">
+            <span>ORIGIN</span>
+            <p>${character.origin.name}</p>
+          </div>
+        </div>
+      </div>
+      `;
+      const contents = document.getElementById("content");
+      contents.innerHTML += characterCard;
+    });
+  } catch (error) {
+    console.error("Error displaying random characters:", error);
+  }
 }
 
-displayRandomCharacters();
+// Entry point to fetch data and display random characters
+(async () => {
+  try {
+    await displayDataCounts();
+    await displayRandomCharacters();
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+})();
